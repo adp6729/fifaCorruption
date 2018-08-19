f79e932a8efdc0b35014f18934c3b6cf59394c8a
Andrew Pittman <apittman2@wisc.edu>
2018-08-19 16:29:39 -0400

    cartogram attempt 1

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ const widthBody = parseInt(body.style("width"))
 const width = parseInt(container.style("width")) - 30
 const height = width / 2.05
 
-const projection = d3.geoNaturalEarth1() // projection used for the mercator projection
+// const projection = d3.geoNaturalEarth1() // projection used
+const projection = d3.geoAlbers() // projection used
     .center([10, 5])
     .scale(width/6)
     .translate([width / 2, height / 2])
@@ -36,33 +37,39 @@ var svg = d3.select("#Map")
 const countriesG = svg.append('g')
     .attr('class', 'countries')
 
+// initialize cartogram
+var cartogram = d3.cartogram()
+    .projection(projection)
+    .value(function(d) {
+        return Math.random() * 100;
+    })
 
-    //create slider
-    var dataSlider = [1996,1998,2000,2002,2004,2006,2008,2010,2012,2014,2016,2018];
+//create slider
+var dataSlider = [1996,1998,2000,2002,2004,2006,2008,2010,2012,2014,2016,2018];
 
-      var slider = d3.sliderHorizontal()
-        .min(d3.min(dataSlider))
-        .max(d3.max(dataSlider))
-        .step(2)
-        .width(800)
-        .tickFormat(d3.format('.0f'))
-        .tickValues(dataSlider)
-        .on('onchange', val => {
-          d3.select("p#value").text((val));
-        });
+var slider = d3.sliderHorizontal()
+    .min(d3.min(dataSlider))
+    .max(d3.max(dataSlider))
+    .step(2)
+    .width(800)
+    .tickFormat(d3.format('.0f'))
+    .tickValues(dataSlider)
+    .on('onchange', val => {
+        d3.select("p#value").text((val));
+    });
 
-      var g = d3.select("#slider")
-        .append("div")
-        .append("svg")
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 5 1100 90")
-        .append("g")
-        .attr("transform", "translate(30,30)");
+var g = d3.select("#slider")
+    .append("div")
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 5 1100 90")
+    .append("g")
+    .attr("transform", "translate(30,30)");
 
-      g.call(slider);
+    g.call(slider);
 
-      d3.select("p#value").text(slider.value());
-      d3.select("a#setValue").on("click", () => slider.value(data));
+    d3.select("p#value").text(slider.value());
+    d3.select("a#setValue").on("click", () => slider.value(data));
 
 
 // Handle data initialization
@@ -174,7 +181,7 @@ Promise.all([
 function processData(results) {
     const geoJson = topojson.feature(results[0],results[0].objects.ne_50m_admin_0_countries_lakes)
     const cData = results[1]
-    var africaArray = []
+    var countryArray = []
     for (const feature of geoJson.features) {
         if (feature.properties.CONTINENT != "Antarctica") {
             for (const stat of cData) {
@@ -188,25 +195,26 @@ function processData(results) {
                     break
                 }
             }
-            africaArray.push(feature)
+            countryArray.push(feature)
         }
     }
     colorScale.domain(d3.extent(cData, d=>d.CorruptionPerceptionIndex2015))
     window.cData = cData // globalize
-    window.africaArray = africaArray // globalize
+    window.countryArray = countryArray // globalize
     //console.log(cData)
-    return africaArray
+    return countryArray
 
 }
 
-function createMap(africaArray) {
+function createMap(countryArray) {
     countriesG
        .selectAll('path')
-       .data(africaArray)
+       .data(countryArray)
        .enter()
           .append('path')
              .attr('class', d => 'country ' + d.properties.ISO_A2)
-             .attr('d', pathGenerator)
+            //  .attr('d', pathGenerator)
+             .attr('d', cartogram.path)
              .style('fill', d => {
                 if (d.properties.CorruptionPerceptionIndex2015) {
                    return colorScale(d.properties.CorruptionPerceptionIndex2015)
@@ -215,7 +223,7 @@ function createMap(africaArray) {
              .on("mousemove", moveToolTip)
              .on("mouseout", hideToolTip)
 
-    return africaArray
+    return countryArray
  }
 
  function moveToolTip(d) {
