92dd9470e90916986dbbda15d12844472d708bd4
Vlad Sliusar <31670686+vladsliusar@users.noreply.github.com>
2018-08-24 21:54:30 -0700

    Trying different color combination for the map

--- js/main.js
+++ js/main.js
@@ -177,7 +177,7 @@ buttonDivs.append("button")
 const transitionDuration = 1000
 
 // GI color scale for countries who didn't make it into the world cup
-const colorScaleGIOut = d3.scaleSequential(d3.interpolateViridis)
+const colorScaleGIOut = d3.scaleSequential(d3.interpolateRdGy)
     // .range(['#a50f15', '#fee5d9']) // this needs tweaking
 
 // GI color scale for countries who make it into the world cup
